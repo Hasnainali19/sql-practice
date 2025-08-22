ad1e0e1aea57cb529219f0b0af9b4917a45a43dd
Hasnainali19 <132987843+Hasnainali19@users.noreply.github.com>
2025-08-22 13:50:51 -0400

    Add files via upload

---
+++ WSDA_Music.sql
@@ -0,0 +1,12 @@
+/*
+Name : Hasnainali Saiyed
+Date : 21-08-2025
+Description : Query to display all customers First,Last Names and Email Address
+*/
+
+SELECT
+	FirstName AS "Customer FirstName",
+	LastName AS "Customer LastName",
+	Email AS EMAIL
+FROM
+	Customer
